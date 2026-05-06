41d35fdfa59d33e8078d206f244a598474831f3d
666Alice666 <margosatanovskaya@gmail.com>
2026-05-06 21:47:13 +0300

    навигация

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def registr():
 def main():
     return render_template('main.html')
 
+@app.route("/history")
+def history():
+    return render_template('history.html')
+
+@app.route("/statistics")
+def statistics():
+    return render_template('statistics.html')
+
+@app.route("/debts")
+def debts():
+    return render_template('debts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
